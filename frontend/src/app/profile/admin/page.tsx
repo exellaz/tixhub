266ab986c54ec0164b9fc56d979ad2a6418e4c28
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-10-04 18:02:35 +0800

    refactor(admin): refactored event form from `page.tsx` into component in `EventForm.tsx`

--- frontend/src/app/profile/admin/page.tsx
+++ frontend/src/app/profile/admin/page.tsx
@@ -1,45 +1,19 @@
 "use client"
 
 import React from 'react'
-import styled from 'styled-components'
+import EventForm from './EventForm'
 
-export const CreateEvent = () => {
-  return (
-	<div>
-		<CenteredContainer>
-			<h1>New Event</h1>
-			<p>Event Name:</p>
-			<Input type='text' placeholder='Enter event name'></Input>
-			<p>Date:</p>
-			<Input type='date'></Input>
-			<p>Venue:</p>
-			<Input type='text' placeholder='Enter venue'></Input>
-			<p>Description:</p>
-			<Input type='text' placeholder='Enter description'></Input>
-			<p>Organiser Address:</p>
-			<Input type='text' placeholder='Enter organiser address'></Input>
-		</CenteredContainer>
-	</div>
-  )
+export const CreateEventPage = () => {
+	
+	const handleEventCreate = (eventData) => {
+		console.log('Event Data:', eventData)
+	}
+
+	return (
+		<div>
+			<EventForm onSubmit={handleEventCreate} />
+		</div>
+	)
 }
 
-export default CreateEvent
-
-const CenteredContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  text-align: center;
-`
-
-const Input = styled.input`
-	display: block;
-	margin-bottom: 10px;
-	padding: 8px;
-	font-size: 16px;
-	width: 100%;
-	max-width: 400px;
-	color: blue;
-`
+export default CreateEventPage
