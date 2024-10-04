a3694dc2bcdf6cf98952ee59f809470ab987e9c9
wiwihhy <yoyowinnie3@gmail.com>
2024-10-04 16:42:15 +0800

    feat (buyer) : added profile page

---
+++ frontend/src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+"use client"; //need because of styled-components
+import styled from 'styled-components';
+import Layout from '../layout';
+import { useState, useEffect } from 'react';
+
+export default function EventPage() {
+
+    return (   
+        <div>
+          <h1>Profile Page</h1>
+        </div>    
+    );
+}
