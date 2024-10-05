d2f096207c5700d4d55e555aeff11b92e4e984de
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-10-05 14:17:37 +0800

    wip(profile): trying to pull data from smart contract

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,13 +1,35 @@
 "use client"; //need because of styled-components
+import Web3, { AbiError } from 'web3';
 import styled from 'styled-components';
 import Layout from '../layout';
 import { useState, useEffect } from 'react';
+import { CONTRACT_ADDRESS } from '@/component/contractAddress';
+import { comment } from 'postcss';
+import ABI from '../../component/ABI.json'
 
-export default function EventPage() {
+const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545')
+const contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS)
 
-    return (   
+export default function ProfilePage() {
+	// TODO
+	// retrieve user tickets info
+	callSmartContractFunction()
+	// render each ticket as a card
+	// when the card is clicked, prompt the user to sell
+    return (
         <div>
           <h1>Profile Page</h1>
-        </div>    
+        </div>
     );
+}
+
+async function callSmartContractFunction() {
+	try {
+	  const accounts = await web3.eth.getAccounts();
+	  const defaultAccount = accounts[0]
+	  const result = await contract.methods.getTokensOfUser(defaultAccount).call({ from: accounts[0] });
+	  console.log('Profile Result:', result);
+	} catch (error) {
+	  console.error('Error calling smart contract function:', error);
+	}
 }
